ab2bb1c1a6e0dba6f431849315dce910608f3250
castchise <rayweoweo@gmail.com>
2025-01-12 23:45:22 +0200

    add custom duration formatter

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
+/* eslint-disable import/no-named-as-default-member */
 import { LocalStorageKey, TodoListItem } from "@/types";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import duration from "dayjs/plugin/duration";
+import dayjs from "dayjs";
+
+dayjs.extend(duration);
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -20,4 +25,16 @@ export function getIsDarkmode() {
   return localStorage.getItem("darkmode")
     ? JSON.parse(localStorage.getItem("darkmode"))
     : false;
+}
+
+export function formatDurationToHours(seconds: number) {
+  const d = dayjs.duration(seconds, "seconds");
+
+  const hours = Math.floor(d.asHours());
+  const minutes = d.minutes();
+  const secondsFormatted = d.seconds();
+
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${secondsFormatted.toString().padStart(2, "0")}`;
 }
