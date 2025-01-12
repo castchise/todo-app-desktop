43272009977582250a25b9cbe333dc1e52e92684
castchise <rayweoweo@gmail.com>
2025-01-12 18:40:23 +0200

    adjust imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
@@ -7,7 +7,7 @@ const rootDiv = document.createElement("div");
 rootDiv.setAttribute("id", "root");
 document.body.appendChild(rootDiv);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
