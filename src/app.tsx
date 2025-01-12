70cad6090147a9aec7f1b7c54102104d12b4dbb2
castchise <rayweoweo@gmail.com>
2025-01-12 17:55:52 +0200

    scaffold task form component

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,9 @@
+import { NewTaskForm } from "@/components/NewTaskForm";
+
 export default function App() {
   return (
-    <div className="w-full h-screen bg-slate-300 dark:bg-slate-500">React</div>
+    <div className="w-full h-screen bg-slate-300 dark:bg-slate-500 p-8">
+      <NewTaskForm />
+    </div>
   );
 }
---
+++ src/components/NewTaskForm.tsx
@@ -0,0 +1,56 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  task: z.string(),
+});
+
+export function NewTaskForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      task: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="task"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Task</FormLabel>
+              <FormControl>
+                <Input placeholder="Task..." {...field} />
+              </FormControl>
+              <FormDescription>Task should be placed here.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
