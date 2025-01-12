1da23360a562f29a01cc624afa3dc3ef5e70e978
castchise <rayweoweo@gmail.com>
2025-01-12 19:01:26 +0200

    add get/set function for localstorage values

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
+import { TodoListItem } from "@/types";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function getTodoList(): TodoListItem[] | null {
+  return localStorage.getItem("todoList")
+    ? JSON.parse(localStorage.getItem("todoList"))
+    : [];
+}
+
+export function setTodoList(todoList: TodoListItem[]) {
+  localStorage.setItem("todoList", JSON.stringify(todoList));
 }
