b8fb55e61879fd1446a39f53926bb1399ab16d7b
castchise <rayweoweo@gmail.com>
2025-01-12 19:49:53 +0200

    utilize scroll area for todo items list

--- src/components/TodoItemList.tsx
+++ src/components/TodoItemList.tsx
@@ -1,12 +1,22 @@
 import type { TodoListItem } from "@/types";
 import TodoItem from "@/components/TodoItem";
+import { cn } from "@/lib/utils";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface TodoItemListProps {
   todoList: TodoListItem[];
+  className?: string;
 }
 
-export default function TodoItemList({ todoList }: TodoItemListProps) {
-  return todoList.map((todoListItem) => (
-    <TodoItem key={todoListItem.id} {...todoListItem} />
-  ));
+export default function TodoItemList({
+  todoList,
+  className,
+}: TodoItemListProps) {
+  return (
+    <ScrollArea className={cn("h-screen", className)}>
+      {todoList.map((todoListItem) => (
+        <TodoItem key={todoListItem.id} className="mb-4" {...todoListItem} />
+      ))}
+    </ScrollArea>
+  );
 }
