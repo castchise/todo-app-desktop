5d1dc6b8405d466f4825e64ceddb77590778284d
castchise <rayweoweo@gmail.com>
2025-01-19 01:31:38 +0200

    adjust markup for settings

--- src/components/app-settings/RemoveAllTodoItemsDialog.tsx
+++ src/components/app-settings/RemoveAllTodoItemsDialog.tsx
@@ -2,18 +2,24 @@ import { useState } from "react";
 import ConfirmationDialog from "../ConfirmationDialog";
 import { Button } from "../ui/button";
 import { useGlobalContext } from "@/contexts";
+import { Trash2 } from "lucide-react";
 
 export function RemoveAllTodoItemsDialog() {
   const [isRemovingItems, setIsRemovingItems] = useState(false);
   const { todoList, removeAllTodoItems } = useGlobalContext();
   return (
     <>
-      <Button
-        disabled={todoList.length === 0}
-        onClick={() => setIsRemovingItems(true)}
-      >
-        Remove all
-      </Button>
+      <div className="flex items-center">
+        <Button
+          disabled={todoList.length === 0}
+          onClick={() => setIsRemovingItems(true)}
+          variant="ghost"
+          size="icon"
+        >
+          <Trash2 color="red" />
+        </Button>
+        <p className="font-semibold ml-2 text-red-500">Remove all Tasks</p>
+      </div>
 
       <ConfirmationDialog
         open={isRemovingItems}
